604cb8ee8a010a9f5db536f781a4448ccbcb9244
Jimmy <jimmyskripchuk@gmail.com>
2020-04-17 15:23:42 -0400

    Updates

---
+++ alpha/precache-manifest.78f00dd0bf4b8bfd4293793babc37b22.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphafavicon.ico"
+  },
+  {
+    "revision": "77f9d3e2315169b95ff4",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphagen.main.426ec7cf92d30b8f424c.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "4dfc413c2e775578a6e8bc15e329659e",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphasw.js"
+  }
+]);
--- alpha/sw.js
+++ alpha/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.9bbb2f9225da99ec00cb98ee8d5d5428.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/alphaprecache-manifest.78f00dd0bf4b8bfd4293793babc37b22.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
