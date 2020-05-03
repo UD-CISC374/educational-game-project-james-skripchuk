63c10cafdb6356773a419104e2587e49b8284b83
Jimmy <jimmyskripchuk@gmail.com>
2020-05-03 17:07:54 -0400

    Updates

---
+++ beta/precache-manifest.aff6513c835a852f230eaef5e807ba27.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "dd207854e30d6444bb6d020e44d32a79",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaassets/styles.css"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betafavicon.ico"
+  },
+  {
+    "revision": "9480aa296f60f6564304",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betagen.main.1a718f107d2af294cfc7.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "9d8a7d84bba65ba17f80f048eb48bf3b",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betaindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/betasw.js"
+  }
+]);
