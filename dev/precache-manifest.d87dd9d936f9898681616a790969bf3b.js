ef950faee9dcd6dc16982562ccf066a4f2222584
Jimmy <jimmyskripchuk@gmail.com>
2020-05-10 20:03:17 -0400

    Updates

---
+++ dev/precache-manifest.d87dd9d936f9898681616a790969bf3b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "dd207854e30d6444bb6d020e44d32a79",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devassets/styles.css"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devfavicon.ico"
+  },
+  {
+    "revision": "be769cc671f6d385004b",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devgen.main.7181665fcf4de76561e1.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devgen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "1b72b6c18ec066da658d707eabab32c1",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devsw.js"
+  }
+]);
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/https://github.com/UD-CISC374/educational-game-project-james-skripchuk/devprecache-manifest.d87dd9d936f9898681616a790969bf3b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
